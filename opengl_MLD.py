d8f186aa5d1289a0305324fa7e2585dda73d9f57
MDAddario <michael.lindner.daddario@gmail.com>
2020-03-19 23:04:02 -0400

    Updates CharacterModel to have collision detection within the class, deletes all traces of ecb

--- opengl_MLD.py
+++ opengl_MLD.py
@@ -63,7 +63,7 @@ def create_vertex_list(batch, vertices, normals, indices, color, render_mode=GL_
 		diffuse = [0.5, 0.0, 0.3, 1.0]
 	else:
 		diffuse = [0.0, 0.0, 0.0, 1.0]
-	
+
 	# Configure formats
 	vertex_format = "v3f/" + vertex_format
 	normal_format = "n3f/" + normal_format
@@ -92,40 +92,40 @@ def triangle_practice(batch):
 	# Create the vertex and normal arrays
 	vertices = []
 	normals = []
-	
+
 	# Populate the vertices array
 	vertices.extend([-size, +size, 0])
 	vertices.extend([-size, -size, 0])
 	vertices.extend([-0, -0, 0])
-	
+
 	vertices.extend([+size, -size, 0])
 	vertices.extend([+size, +size, 0])
 	vertices.extend([+0, +0, 0])
-	
+
 	# Populate the normals array
 	z_normal = [0.1, 0.1, 0.98]
-	
+
 	for i in range(6):
 		normals.extend(z_normal)
-	
+
 	# Create a list of triangle indices
 	indices = []
 	indices.extend([0, 1, 2])
 	indices.extend([3, 4, 5])
 	indices.extend([0, 2, 1])
 	indices.extend([3, 5, 4])
-	
-	return create_vertex_list(batch, vertices, normals, indices, "red", 
+
+	return create_vertex_list(batch, vertices, normals, indices, "red",
 								GL_TRIANGLES, "static", "static")
 
 
 # Create floating rectangles
 def box_creator(batch, size, center, color, vertex_format, normal_format):
-	
+
 	# Convert to array objects
 	size = np.asarray(size)
 	center = np.asarray(center)
-	
+
 	# Error check
 	if np.any(size <= 0):
 		raise ValueError("Size values must be strictly positive")
@@ -133,73 +133,73 @@ def box_creator(batch, size, center, color, vertex_format, normal_format):
 		raise ValueError("Size must be a 1D array, 3 in length")
 	if center.shape != (3,):
 		raise ValueError("Center must be a 1D array, 3 in length")
-	
+
 	# Offset parameter
 	offset = center - size / 2
-	
+
 	# Create the vertex and normal arrays
 	vertices = []
 	normals = []
-	
+
 	# Have some standard normals
 	x_normal = np.array([0.98, 0.1, 0.1])
 	y_normal = np.array([0.1, 0.98, 0.1])
 	z_normal = np.array([0.1, 0.1, 0.98])
-	
+
 	# Front face
 	vertices.extend(np.array([0, 0, 1] * size + offset))
 	vertices.extend(np.array([1, 0, 1] * size + offset))
 	vertices.extend(np.array([1, 1, 1] * size + offset))
 	vertices.extend(np.array([0, 1, 1] * size + offset))
-	
+
 	for i in range(4):
 		normals.extend(z_normal)
-	
+
 	# Back face
 	vertices.extend(np.array([0, 0, 0] * size + offset))
 	vertices.extend(np.array([0, 1, 0] * size + offset))
 	vertices.extend(np.array([1, 1, 0] * size + offset))
 	vertices.extend(np.array([1, 0, 0] * size + offset))
-	
+
 	for i in range(4):
 		normals.extend(-z_normal)
-	
+
 	# Top face
 	vertices.extend(np.array([0, 1, 0] * size + offset))
 	vertices.extend(np.array([0, 1, 1] * size + offset))
 	vertices.extend(np.array([1, 1, 1] * size + offset))
 	vertices.extend(np.array([1, 1, 0] * size + offset))
-	
+
 	for i in range(4):
 		normals.extend(y_normal)
-	
+
 	# Bot face
 	vertices.extend(np.array([0, 0, 0] * size + offset))
 	vertices.extend(np.array([1, 0, 0] * size + offset))
 	vertices.extend(np.array([1, 0, 1] * size + offset))
 	vertices.extend(np.array([0, 0, 1] * size + offset))
-	
+
 	for i in range(4):
 		normals.extend(-y_normal)
-	
+
 	# Right face
 	vertices.extend(np.array([1, 0, 0] * size + offset))
 	vertices.extend(np.array([1, 1, 0] * size + offset))
 	vertices.extend(np.array([1, 1, 1] * size + offset))
 	vertices.extend(np.array([1, 0, 1] * size + offset))
-	
+
 	for i in range(4):
 		normals.extend(x_normal)
-	
+
 	# Left face
 	vertices.extend(np.array([0, 0, 0] * size + offset))
 	vertices.extend(np.array([0, 0, 1] * size + offset))
 	vertices.extend(np.array([0, 1, 1] * size + offset))
 	vertices.extend(np.array([0, 1, 0] * size + offset))
-	
+
 	for i in range(4):
 		normals.extend(-x_normal)
-	
+
 	# Do the indices too
 	indices = []
 	for i in range(len(vertices)):
@@ -211,7 +211,7 @@ def box_creator(batch, size, center, color, vertex_format, normal_format):
 
 # Create a set of vertex lists for battlefield stage
 def battlefield_creator(batch, color="blue"):
-	
+
 	# Keep track of all platforms
 	vertex_lists = []
 
@@ -222,96 +222,86 @@ def battlefield_creator(batch, color="blue"):
 	left_center  = [-5.0, 3.0, 0.0]
 	right_center = [ 5.0, 3.0, 0.0]
 	top_center   = [ 0.0, 5.0, 0.0]
-	
+
 	# Create all platforms
 	vertex_lists.append(box_creator(batch, base_size, base_center,  color, "static", "static"))
 	vertex_lists.append(box_creator(batch, plat_size, left_center,  color, "static", "static"))
 	vertex_lists.append(box_creator(batch, plat_size, right_center, color, "static", "static"))
 	vertex_lists.append(box_creator(batch, plat_size, top_center,   color, "static", "static"))
-	
+
 	# Dead dictionary
 	dead = {key.W: False, key.A: False, key.S: False, key.D: False}
-	
+
 	# Compute CharacterModel object for each box
 	model_list = []
 	for vertex_list in vertex_lists:
-		model_list.append(CharacterModel(batch, vertex_list, dead))
-	
+		model_list.append(CharacterModel(batch, vertex_list, dead, []))
+
 	return model_list
 
 
 # Class to keep track of model and associated attributes
 class CharacterModel:
 
+	# Classical mechanics attributes
+	force     = 250.0
+	friction  =  45.0
+	max_speed =  10.0
+
 	# Constructor
-	def __init__(self, batch, vertex_list, keys, center=None, ecb_dims=None):
-		
-		# Store as instance attributes
-		self.batch = batch
-		self.vertex_list = vertex_list
-		self.keys = keys
+	def __init__(self, batch, vertex_list, keys, stage_model_list):
+
+		# Store the instance attributes
+		self.batch            = batch
+		self.vertex_list      = vertex_list
+		self.keys             = keys
+		self.stage_model_list = stage_model_list
 
 		# Extract a deepcopy of vertices formatted as Nx3 array
 		self.vertices = []
 		self.vertices.extend(self.vertex_list.vertices)
 		self.vertices = np.reshape(self.vertices, (-1, 3))
 
-		# Define classical mechanics
-		self.force = 250.0
-		self.friction = 45.0
-		self.max_speed = 10.0
+		# Compute transformation center and Environment Collision Box dimensions
+		self.center   = self.__compute_center()
+		self.ecb_dims = self.__compute_ecb_dims()
+
+		# Start body at rest
 		self.velocity = np.zeros(3)
-		
-		# Include the center of all coordinate transforms
-		if center is not None:
-			self.center = center
-		else:
-			self.__compute_center()
-		
-		# Determine the environment collision box
-		if ecb_dims is not None:
-			self.ecb_dims = ecb_dims
-		else:
-			self.__compute_ecb_dims()
-	
-	# Determine model center from the vertices
-	def __compute_center(self):
-		self.center = np.max(self.vertices, axis=0) + np.min(self.vertices, axis=0)
-		self.center /= 2
-	
-	# Determine ecb dimensions from the vertices
-	def __compute_ecb_dims(self):
-		self.ecb_dims = np.max(self.vertices, axis=0) - np.min(self.vertices, axis=0)
-		self.ecb_dims /= 2
-	
+
 	# Destructor
 	def __delete__(self):
 		vertex_list.delete()
-		del self.ecb
+
+	# Determine model center from the vertices
+	def __compute_center(self):
+		return (np.max(self.vertices, axis=0) + np.min(self.vertices, axis=0)) / 2
+
+	# Determine ecb dimensions from the vertices
+	def __compute_ecb_dims(self):
+		return (np.max(self.vertices, axis=0) - np.min(self.vertices, axis=0)) / 2
+
+	# Update the real vertex list from the object's local copy
+	def __update_vertex_list(self):
+		self.vertex_list.vertices = np.copy(np.ravel(self.vertices))
 
 	# Scale both the real, local vertices, and the ecb dimensions
 	def __scale_vertices(self, scaling):
 		self.vertices *= scaling
-		self.vertex_list.vertices = np.copy(np.ravel(self.vertices))
+		self.__update_vertex_list()
 		self.ecb_dims *= scaling
-	
+
 	# Rotate both the real, local vertices, and the ecb dimensions
 	def __rotate_vertices(self, rotation):
 		self.vertices = rotation.apply(self.vertices)
-		self.vertex_list.vertices = np.copy(np.ravel(self.vertices))
-		self.__compute_ecb_dims()
+		self.__update_vertex_list()
+		self.ecb_dims = self.__compute_ecb_dims()
 
 	# Translate both the real, local vertices, and the transformation center
 	def __translate_vertices(self, translation):
 		self.vertices += translation
-		self.vertex_list.vertices = np.copy(np.ravel(self.vertices))
+		self.__update_vertex_list()
 		self.center += translation
-	
-	# For debugging, show the ECB of the object
-	def show_ecb(self):
-		vertex_list = box_creator(self.batch, 2 * self.ecb_dims, self.center, 
-									"yellow", "dynamic", "static")
-		self.ecb = CharacterModel(self.batch, vertex_list, self.keys)
 
 	# Rescale total object size about center
 	def rescale(self, new_size):
@@ -351,7 +341,7 @@ class CharacterModel:
 	def set_position(self, position):
 		self.__translate_vertices(position - self.center)
 
-	# Update model position based off keyboard input
+	# Update model position based off keyboard input, existing velocity, and evironment coliisions
 	def update(self, dt):
 
 		# Drive the system by user input
@@ -363,16 +353,38 @@ class CharacterModel:
 			self.velocity[0] += self.force * dt
 		if self.keys[key.A]:
 			self.velocity[0] -= self.force * dt
-		
+
 		# Account for friction
 		self.velocity -= np.sign(self.velocity) * self.friction * dt
-		
-		# Zero snap for friction
+
+		# Allow friction to bring bodies to rest
 		self.velocity = np.where(np.abs(self.velocity) - self.friction * dt < 0, 0, self.velocity)
-		
-		# Force maximum velocity
+
+		# Enforce maximum velocity
 		np.clip(self.velocity, -self.max_speed, self.max_speed, out=self.velocity)
-		
+
+		# Clip detection
+		for stage_model in self.stage_model_list:
+
+			separation = np.abs(stage_model.center[0:2] - self.center[0:2]) \
+							- (stage_model.ecb_dims[0:2] + self.ecb_dims[0:2])
+
+			# Check for negative separations
+			if np.all(separation < 0):
+
+				# Decide along which axis to eject the body
+				xi = np.argmax(separation)
+
+				# Eject body
+				new_position = np.copy(self.center)
+				sign = np.sign(self.center[xi] - stage_model.center[xi])
+				displacement = self.ecb_dims[xi] + stage_model.ecb_dims[xi]
+				new_position[xi] = stage_model.center[xi] + sign * displacement
+				self.set_position(new_position)
+
+				# Set velocity to zero
+				self.velocity[xi] = 0
+
 		# Move player for non-zero velocity
 		if not np.allclose(self.velocity, 0):
 			self.__translate_vertices(dt * self.velocity)
@@ -467,33 +479,13 @@ def on_mouse_scroll(x, y, scroll_x, scroll_y):
 
 # Update every frame
 def update(dt):
-	
+
 	# Update all mobile objects
 	for obj in object_list:
 		obj.update(dt)
-	
-	# Clip detection
-	for stage_model in stage_model_list:
-		
-		separation = np.abs(stage_model.center[0:2] - fox_model.center[0:2]) \
-						- (stage_model.ecb_dims[0:2] + fox_model.ecb_dims[0:2])
-		
-		# Check for negative separations
-		if np.all(separation < 0):
-		
-			# Decide along which axis to eject the body
-			xi = np.argmax(separation)
-			
-			# Eject body
-			new_position = np.copy(fox_model.center)
-			sign = np.sign(fox_model.center[xi] - stage_model.center[xi])
-			displacement = stage_model.ecb_dims[xi] + fox_model.ecb_dims[xi]
-			new_position[xi] = stage_model.center[xi] + sign * displacement
-			fox_model.set_position(new_position)
-			
-			# Set velocity to zero
-			fox_model.velocity[xi] = 0
 
+
+# The main attraction
 if __name__ == "__main__":
 
 	# Setup window and batch
@@ -523,22 +515,16 @@ if __name__ == "__main__":
 	# Load 3D fox model
 	os.chdir('fox/')
 	fox = pyglet.model.load("low-poly-fox-by-pixelmannen.obj", batch=batch)
-	fox_model = CharacterModel(batch, fox.vertex_lists[0], keys)
+	fox_model = CharacterModel(batch, fox.vertex_lists[0], keys, stage_model_list)
 
 	# Configure initial conditions for fox model
 	fox_model.rescale(2)
 	fox_model.set_position([-5, 1.2, 0])
 	fox_model.rotate_degrees('y', 90)
-	fox_model.show_ecb()
-	
-	# Configure I.C.s for stage model
-	for stage_model in stage_model_list:
-		stage_model.show_ecb()
 
 	# Keep track of all the active models
 	object_list = []
 	object_list.append(fox_model)
-	object_list.append(fox_model.ecb)
 
 	# Run the animation!
 	pyglet.app.run()
